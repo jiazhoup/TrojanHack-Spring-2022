1c54c35c055a679bca6ff7cfdb10f100f71724db
unknown <matthiaskrh123@gmail.com>
2022-04-23 15:07:52 -0700

    Organizer done I think

--- organizer.py
+++ organizer.py
@@ -1,5 +1,7 @@
 import os
-import node
+from node import Node
+from os import walk
+import re
 
 
 """
@@ -8,7 +10,12 @@ rootNode: The root node of a Node data structure representing a policy
 Reads the Node data structure and organizes the desktop according to it
 """
 def organizeDesktop(rootNode):
-    return
+
+    desktopPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+
+    # Organizing all children that are not the desktop node
+    for child in Node.getChildren(rootNode):
+        organize(child, desktopPath, desktopPath)
 
 
 
@@ -21,15 +28,35 @@ parentDirectory: The path to the directory that the new folder and all nested on
 
 A recrusive function that creates a folder structure on the way down and organizes on the way back up
 """  
-def organize(node, parentDirectory):
-    newDirectoryPath = parentDirectory + "/" + node.getName()
+def organize(node, parentDirectory, desktopPath):
+    # Create new folder for node object
+    newDirectoryPath = os.path.join(parentDirectory, node.getName())
     os.mkdir(newDirectoryPath)
 
-    for regex in node.:
+    # Search desktop for files that satisfy each accepting regex
+    desktopFilenames = next(walk(desktopPath), (None, None, []))[2]  # [] if no file
+    for regex in Node.getAcceptedRegexs(node):
 
-    for child in node.getChildren():
-        organize(child, newDirectoryPath)
+        # Moving files to the directory path that match the regex
+        for matchedFile in getMatchingStrings(desktopFilenames, regex):
+            os.rename(os.path.join(desktopPath, matchedFile), os.path.join(newDirectoryPath, matchedFile))
 
+
+    # Recursively organize all children folders
+    for child in Node.getChildren(node):
+        organize(child, newDirectoryPath, desktopPath)
+
+
+
+"""
+Returns the subset of the list of strings that match the regex
+"""
+def getMatchingStrings(listOfStrings, regex):
+    out = []
+    for string in listOfStrings:
+        if re.match(regex, string):
+            out.append(string)
+    return out
 
     
 
