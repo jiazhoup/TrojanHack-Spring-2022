eacc08c83b25e2de6f7ce4a233d01b28d40c474d
jiazhoup <pangjiazhou@gmail.com>
2022-04-24 08:49:25 -0700

    new ui

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sys
 from unittest.mock import DEFAULT
 import os
@@ -9,14 +8,15 @@ import tokenizer
 import tkinter as tk
 from PIL import Image, ImageTk
 
-DEFAULT_POLICY_PATH = "default_policy"
+DEFAULT_POLICY_PATH = "default_policy.txt"
+
 
 def organize():
     # fileName = sys.argv[1]
 
     fileName = e.get()  # Get file name from text entry
 
-    if fileName == None:
+    if fileName == 'Enter policy file name':
         fileName = DEFAULT_POLICY_PATH
 
     """
@@ -44,6 +44,9 @@ def organize():
         pickle.dump(moveHistory, fp)
 
 
+"""
+Unpickle the file containing moveHistory. 
+"""
 def keepChanges():
     with open('moveHistory', 'rb') as fp:  # Unpickling
         moveHistory = pickle.load(fp)
@@ -54,51 +57,70 @@ def keepChanges():
     os.remove('moveHistory')
 
 
-
 """
 Window object 
 """
 root = tk.Tk()
 root.geometry("300x400")
+root.iconbitmap('Images/file_archive_folders_icon_193943.ico')
+root.title('Desktop Org')
 
 
 """
 Background
 """
-bg_color = '#66FFB2'
+bg_color = '#484848'
 root.configure(background=bg_color)
 
 
 """
-Organize button. Calls the organize function
+Logo?
 """
-btn_img = Image.open('Images/Mamluk Sultanate.png')
-btn_img.thumbnail((100, 100))   # Rescale image
-btn_img = ImageTk.PhotoImage(btn_img)
-btn_label = tk.Label(image=btn_img)
-
-btn = tk.Button(root, image=btn_img, command=organize, bd=0, bg=bg_color)
-btn.pack(padx=(0, 0), pady=(120, 0))
-
+logo = Image.open('Images/placeholder.jpg')
+logo.thumbnail((100, 100))  # Rescale image
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(root, image=logo)
+tk.Button(root, image=logo).pack(side='top', pady=(100, 0))
 
 """
 Text entry
 """
 e = tk.Entry(root, width=25, fg='gray', bd=0, bg='#f2f2f2', font='consolas 8')
-e.pack(pady=(20, 0))
+e.pack(pady=(50, 0))
 e.insert(0, 'Enter policy file name')
 
 
 """
-Revert button. Calls the organize function
+Revert button. Calls keepChanges function
 """
-btn_img2 = Image.open('Images/back.jpg')
-btn_img2.thumbnail((40, 40))   # Rescale image
+btn_img2 = Image.open('Images/goback.png')
+btn_img2.thumbnail((55, 55))  # Rescale image
 btn_img2 = ImageTk.PhotoImage(btn_img2)
 btn_label2 = tk.Label(image=btn_img2)
 
 btn2 = tk.Button(root, image=btn_img2, command=keepChanges, bd=0, bg=bg_color)
-btn2.pack(padx=(100, 0), pady=(10, 0))
+btn2.pack(padx=(73, 0),
+          pady=(10, 0),
+          expand=False,
+          fill='x',
+          side='left',
+          anchor='ne')
 
+
+"""
+Organize button. Calls the organize function
+"""
+btn_img = Image.open('Images/change.png')
+btn_img.thumbnail((55, 55))  # Rescale image
+btn_img = ImageTk.PhotoImage(btn_img)
+btn_label = tk.Label(image=btn_img)
+
+btn = tk.Button(root, image=btn_img, command=organize, bd=0, bg=bg_color)
+btn.pack(padx=40,
+         pady=10,
+         expand=False,
+         fill='x',
+         side='left',
+         anchor='n')
 
 root.mainloop()
--- organizer.py
+++ organizer.py
@@ -36,14 +36,6 @@ def organizeDesktop(rootNode):
     print("Move log outputted to " + output_path)
 
     return moveHistory
-    # while(True):
-    #     decision = input("Do you wish to keep the changes (Y/N)")
-    #     if decision == "Y":
-    #         break
-    #     elif decision == "N":
-    #         for move in moveHistory:
-    #             os.rename(move[1], move[0])
-    #         break
 
   
 """
